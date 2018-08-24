f661cea5f8d1d972c9466e9e503d21d1d9ee2681
Pooya Hatami <pooya@node-1w7jr9qro06sw04hcipbybhw2.ipv6.telus.net>
2018-08-24 16:37:03 -0700

    done

--- IOS-Swift-ShakeGesture/ViewController.swift
+++ IOS-Swift-ShakeGesture/ViewController.swift
@@ -15,9 +15,19 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func becomeFirstResponder() -> Bool {
+        return true
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        if motion == .motionShake
+        {
+            let alertController = UIAlertController(title: nil, message:
+                "Shake Gesture Detected", preferredStyle: UIAlertControllerStyle.alert)
+            alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default,handler: nil))
+            
+            self.present(alertController, animated: true, completion: nil)
+        }
     }
 
 
